c8bde2328754e1d2d88385d4654f1bbf83ca37af
kandirou mohamed <105118673+med-kandirou@users.noreply.github.com>
2023-11-24 15:25:36 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'you-Quiz';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
