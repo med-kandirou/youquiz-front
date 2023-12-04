3de3e66ff48e0e4b277ecc38752e9e9ef94910e8
kandirou mohamed <105118673+med-kandirou@users.noreply.github.com>
2023-12-04 09:42:24 +0100

    update

--- src/app/modules/student/components/result-student/result-student.component.ts
+++ src/app/modules/student/components/result-student/result-student.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Assignment } from 'src/app/core/models/Assignment.model';
+import { STUDENT_ID } from 'src/app/core/services/api.config';
 
 @Component({
   selector: 'app-result-student',
